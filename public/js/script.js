932bed96aace53554f5604d090eb7b67b2d601c0
Sunny <isunnyprajapati@gmail.com>
2026-03-06 23:21:49 +0530

    404

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,24 @@
-document.querySelectorAll(".navbtn").addEventListener('click', (event) => 
+const searchBox = document.querySelector('.search-box');
+const searchIcon = document.querySelector('#search-icon');
+
+searchIcon.addEventListener('click', (e) => 
 {
-    console.log(this.id);
+    e.stopPropagation();
+    searchBox.style.display = 'flex';
+});
+document.addEventListener('click', (e) =>
+{
+    if (!searchBox.contains(e.target) && e.target !== searchIcon)
+        searchBox.style.display = 'none';
+});
+
+const homeBtn = document.querySelector('#home');
+const aboutBtn = document.querySelector('#about');
+const linksBtn = document.querySelector('#links');
+
+homeBtn.addEventListener('click', function () 
+{
+    // aboutBtn.classList.remove('active');
+    // linksBtn.classList.remove('active');
+    homeBtn.classList.add('active');
 });
